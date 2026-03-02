decaf8a4821d11f06b20f4b954baaedd10836ceb
sheeko0 <harmanps687@gmail.com>
2026-03-02 12:00:00 +0530

    Day 29: d29.c

---
+++ d29.c
@@ -0,0 +1,95 @@
+/*Problem: Rotate Linked List Right by k Places - Implement using linked list with dynamic memory allocation.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+- Third line: integer k
+
+Output:
+- Print the linked list elements after rotation, space-separated
+
+Example:
+Input:
+5
+10 20 30 40 50
+2
+
+Output:
+40 50 10 20 30
+
+Explanation:
+Connect last node to head forming circular list. Traverse to (n-k)th node, set next to NULL, update head to (n-k+1)th node.*/
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n <= 0)
+        return 0;
+
+    struct Node *head = NULL, *tail = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int val;
+        scanf("%d", &val);
+
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = val;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    int k;
+    scanf("%d", &k);
+
+    k = k % n;
+    if (k == 0) {
+        struct Node* temp = head;
+        while (temp != NULL) {
+            printf("%d", temp->data);
+            if (temp->next != NULL)
+                printf(" ");
+            temp = temp->next;
+        }
+        return 0;
+    }
+
+    tail->next = head;
+
+    int steps = n - k;
+    struct Node* newTail = head;
+
+    for (int i = 1; i < steps; i++)
+        newTail = newTail->next;
+
+    struct Node* newHead = newTail->next;
+    newTail->next = NULL;
+
+    struct Node* temp = newHead;
+    while (temp != NULL) {
+        printf("%d", temp->data);
+        if (temp->next != NULL)
+            printf(" ");
+        temp = temp->next;
+    }
+
+    return 0;
+}
